222e2e03f64f73b3d07efd8228cb045de72605e1
Math.aMalafaia <math.amalafaia@gmail.com>
2026-09-22 18:37:27 -0300

    Feat: conecção com o MySQL funcionando

--- node_sql/index.js
+++ node_sql/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mysql = require('mysql2');    
 const path = require('path');
 
 const app = express();
@@ -19,6 +20,22 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.render('layouts/home');
 });
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '2606',
+    database: 'crud_livros'
+})
+
+conn.connect(function(err) {
+    if (err) {
+        console.log('Erro ao conectar ao Mysql:', err);
+        return;
+    }
+
+    console.log('Conectado ao MySQL!')
+})
 
 app.listen(3000, () => {
     console.log('Servidor rodando em http://localhost:3000');
