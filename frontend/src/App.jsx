d6fe24a3d31e548ae0eeb25e06cc2b9a103c9a68
Math.aMalafaia <math.amalafaia@gmail.com>
2026-09-24 15:52:06 -0300

    Feat: adição do front e do back

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Meu CRUD de livros</h1>
+      <p>Frontend React funcionando</p>
+    </div>
+  )
+}
+
+export default App
